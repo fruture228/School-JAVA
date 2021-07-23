b2b2a20eea382edc223701e25d83aea70698c059
Daewoo <83815936+fruture228@users.noreply.github.com>
2021-07-23 15:37:34 +0300

    Create while.java

---
+++ while.java
@@ -0,0 +1,17 @@
+package com.main;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int countDown = 10;
+
+        while (countDown >= 0) {
+            System.out.println("До старта: " + countDown);
+            countDown --;
+        }
+
+        System.out.println("Поехали !");
+
+    }
+}
